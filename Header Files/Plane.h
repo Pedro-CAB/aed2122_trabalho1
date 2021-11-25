d5e7ef344bc250bf3ba4146d4a3ce69b1d5c5d2c
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2021-11-25 00:09:09 +0000

    Plane Header
    Header Inicial da Classe Plane

--- Header Files/Plane.h
+++ Header Files/Plane.h
@@ -1,1 +1,34 @@
+#include <string>
+#include <list>
+#include <queue>
+#include "Service.h"
+#include "Passenger.h"
+#ifndef UNTITLED1_PLANE_H
+#define UNTITLED1_PLANE_H
 
+using namespace std;
+
+class Plane {
+public:
+    Plane(string LPlate);
+    string LPlate_str, letter;
+    int n, c, max_ocupation, taken_seats;
+    queue<Service> to_do, done;
+    list<Passenger> passengers;
+
+    //Getters:
+    string getLPlate(),getLPlateLetter();
+    int getLPlateNumber(), getN(), getC(), getMaxOccupation(), getTaken_Seats(), getVacantSeats();
+    queue<Service> getToDo(), getDone();
+    //Setters:
+    void setLPlate(string newLPlate),setN(int n),
+    setC(int c), setMaxOc(int num),setTakenS(int num),
+    setToDo(queue<string> q), setDone(queue<string> q);
+    //Edit:
+    void addPassenger(Passenger a);
+    void addTask(Service a);
+    void doNextNTasks(int n);
+};
+
+
+#endif //UNTITLED1_PLANE_H
