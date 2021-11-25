3d451869bd4cfc851f0b22c5e3fe3f9e212525d9
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2021-11-25 11:37:02 +0000

    Passenger.h First Version

--- Header Files/Passenger.h
+++ Header Files/Passenger.h
@@ -1,1 +1,23 @@
+#include <string>
+#include "Ticket.h"
+#include "Flight.h"
+#ifndef UNTITLED1_PASSENGER_H
+#define UNTITLED1_PASSENGER_H
 
+using namespace std;
+
+class Passenger {
+public:
+    string name;
+    Ticket ticket;
+    Passenger(Ticket ticket);
+    Passenger(string name, Ticket ticket);
+    //Getters:
+    string getName();
+    Ticket getTicket();
+    Flight getFlight();
+    bool getLuggage();
+};
+
+
+#endif //UNTITLED1_PASSENGER_H
