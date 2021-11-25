944af263a3a59d1be313e0d526ac73eab8632659
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2021-11-25 11:39:02 +0000

    Ticket.cpp First Version

--- Source Files/Ticket.cpp
+++ Source Files/Ticket.cpp
@@ -1,1 +1,10 @@
-
+#include "Ticket.h"
+Ticket::Ticket(Flight flight){
+    this->flight = flight;
+    AutoLug = false;
+}
+//Getters:
+Flight Ticket::getFlight(){return flight;};
+bool getAutoLug();
+//Setters:
+void setAutoLug(bool state);
