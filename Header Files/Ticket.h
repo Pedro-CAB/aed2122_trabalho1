7f9dc95cc7e589e9fd7835444b06a725d1aaf9e8
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2021-11-25 11:39:51 +0000

    Ticket.h First Version

--- Header Files/Ticket.h
+++ Header Files/Ticket.h
@@ -1,1 +1,15 @@
+class Ticket {
+public:
+    Flight flight;
+    bool AutoLug;
+    Ticket();
+    Ticket(Flight flight);
+    //Getters:
+    Flight getFlight();
+    bool getAutoLug();
+    //Setters:
+    void setAutoLug(bool state);
+};
 
+
+#endif //UNTITLED1_TICKET_H
