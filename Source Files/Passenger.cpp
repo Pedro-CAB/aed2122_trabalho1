93de31ba9ffc9c772f2155bed5f0e6396b711b9e
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2021-11-25 11:37:55 +0000

    Passenger.cpp First Version

--- Source Files/Passenger.cpp
+++ Source Files/Passenger.cpp
@@ -1,1 +1,15 @@
+#include "Passenger.h"
 
+Passenger::Passenger(Ticket ticket){
+    this->ticket = ticket;
+    this->name = "";
+}
+Passenger::Passenger(string name, Ticket ticket){
+    this->ticket=ticket;
+    this->name = name;
+}
+//Getters:
+string Passenger::getName(){return name;}
+Ticket Passenger::getTicket(){return ticket;}
+Flight Passenger::getFlight(){};
+bool getLuggage();
