81d8a05ea4c69ca5cec56d7624dfd2cbea218592
Inês Gaspar <78428139+ines-08@users.noreply.github.com>
2021-11-26 19:21:19 +0000

    Add Flight.cpp

--- Source Files/Flight.cpp
+++ Source Files/Flight.cpp
@@ -1,1 +1,49 @@
+#include "Flight.h"
 
+Flight::Flight(int number, float duration, string date, string origin, string destination){
+    this->number = number,
+    this->duration = duration;
+    this->date = date;
+    this->origin = origin;
+    this->destination = destination;
+}
+
+int Flight::getNumberFlight(){
+    return number;
+}
+
+float Flight::getDurationFlight(){
+    return duration;
+}
+
+string Flight::getDateFlight(){
+    return date;
+}
+
+string Flight::getOrigin(){
+    return origin;
+}
+
+string Flight::getDestination(){
+    return destination;
+}
+
+void Flight::setNumberFlight(int number){
+    this->number = number;
+}
+
+void Flight::setDurationFlight(float duration){
+    this->number = number;
+}
+
+void Flight::setDateFlight(string date){
+    this->date = date;
+}
+
+void Flight::setOrigin(string origin){
+    this->origin = origin;
+}
+
+void Flight::setDestination(string destination){
+    this->destination = destination;
+}
