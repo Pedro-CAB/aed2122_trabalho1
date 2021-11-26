317503d458ca9f20e55ef0a831e051e064aa82d5
Inês Gaspar <78428139+ines-08@users.noreply.github.com>
2021-11-26 19:20:20 +0000

    Add Flight.h

--- Header Files/Flight.h
+++ Header Files/Flight.h
@@ -1,1 +1,25 @@
+#ifndef PROJECTAED_FLIGHT_H
+#define PROJECTAED_FLIGHT_H
 
+#endif //PROJECTAED_FLIGHT_H
+
+#include <string>
+using namespace std;
+
+private class Flight{
+public:
+    int number_flight;
+    float duration_flight;
+    string date_flight, origin, destination;
+    Flight (int number, float duration, string date, string origin, string destination);
+    int getNumberFlight();
+    float getFloatDurationFlight();
+    string getDateFlight();
+    string getOrigin();
+    string getDestination();
+    void setNumberFlight(int number);
+    void setDurationFlight(float duration);
+    void setDateFlight(string date);
+    void setOrigin(string origin);
+    void setDestination(string destination);
+};
