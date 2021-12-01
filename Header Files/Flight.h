75dd2a2b18b38d7a9904fb45974b8cbc86e0cf73
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2021-12-01 15:54:14 +0000

    Default Constructor for Flight Object

--- Header Files/Flight.h
+++ Header Files/Flight.h
@@ -1,25 +1,35 @@
 #ifndef PROJECTAED_FLIGHT_H
 #define PROJECTAED_FLIGHT_H
 
-#endif //PROJECTAED_FLIGHT_H
-
 #include <string>
 using namespace std;
 
-private class Flight{
+class Flight {
 public:
     int number;
     float duration;
     string date, origin, destination;
-    Flight (int number, float duration, string date, string origin, string destination);
+    Flight();
+    Flight(int number, float duration, string date, string origin, string destination);
+
     int getNumber();
+
     float getDuration();
+
     string getDate();
+
     string getOrigin();
+
     string getDestination();
+
     void setNumber(int number);
+
     void setDuration(float duration);
+
     void setDate(string date);
+
     void setOrigin(string origin);
+
     void setDestination(string destination);
 };
+#endif //PROJECTAED_FLIGHT_H
